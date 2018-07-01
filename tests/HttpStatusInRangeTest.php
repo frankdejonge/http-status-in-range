0b2891b48e61074e41f00e5fa08067992d07f5ca
peter279k <peter279k@gmail.com>
2018-07-01 18:26:34 +0800

    Refactor data provider

--- tests/HttpStatusInRangeTest.php
+++ tests/HttpStatusInRangeTest.php
@@ -36,10 +36,12 @@ class HttpStatusInRangeTest extends TestCase
 
     public function statusInRange()
     {
-        yield [HTTP_INFORMATIONAL, [100, 101, 199], [200, 99]];
-        yield [HTTP_SUCCESS, [200, 204, 299], [199, 300]];
-        yield [HTTP_REDIRECTION, [300, 301, 302, 399], [299, 400]];
-        yield [HTTP_CLIENT_ERROR, [400, 401, 403, 499], [399, 500]];
-        yield [HTTP_SERVER_ERROR, [500, 501, 504, 599], [499, 600]];
+        return [
+            [HTTP_INFORMATIONAL, [100, 101, 199], [200, 99]],
+            [HTTP_SUCCESS, [200, 204, 299], [199, 300]],
+            [HTTP_REDIRECTION, [300, 301, 302, 399], [299, 400]],
+            [HTTP_CLIENT_ERROR, [400, 401, 403, 499], [399, 500]],
+            [HTTP_SERVER_ERROR, [500, 501, 504, 599], [499, 600]],
+        ];
     }
 }
